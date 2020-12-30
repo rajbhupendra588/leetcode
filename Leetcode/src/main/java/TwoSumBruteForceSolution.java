9d8afef86eb9a2a640ab6a6e6d96914ab7ed88bc
Bhupendra Singh <bhupendra.singh@appdirect.com>
2020-12-31 01:20:07 +0530

    Update TwoSumBruteForceSolution.java

--- Leetcode/src/main/java/TwoSumBruteForceSolution.java
+++ Leetcode/src/main/java/TwoSumBruteForceSolution.java
@@ -59,3 +59,11 @@ public class TwoSumBruteForceSolution {
 
 }
 
+/*
+Brute force:
+O(n2) runtime, O(1) space – 
+The brute force approach is simple. Loop through each element x and find if there is
+another value that equals to target – x. As finding another value requires looping through
+the rest of array, its runtime complexity is O(n2).
+
+*/
